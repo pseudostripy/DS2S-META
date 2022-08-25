ea56b44adde08dfa82caed8d650c06362e58796f
pseudostripy <pseudostripy@gmail.com>
2022-08-24 21:16:18 -0400

    Initial front-end testing
     - Added randomizer tab, seed textbox and fake checkbox for running
     - No code associated to it yet, was just getting used to META's

---
+++ DS2S META/TabControls/RandomizerControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DS2S_META
+{
+    /// <summary>
+    /// Interaction logic for CovenantControl.xaml
+    /// </summary>
+    public partial class RandomizerControl : METAControl
+    {
+        public RandomizerControl()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //Handles the "Seeed..." label on the text box
+        private void FixSeedVisibility()
+        {
+            if (txtSeed.Text == "")
+                lblSeed.Visibility = Visibility.Visible;
+            else
+                lblSeed.Visibility = Visibility.Hidden;
+
+        }
+
+        private void txtSeed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FixSeedVisibility();
+        }
+    }
+}
