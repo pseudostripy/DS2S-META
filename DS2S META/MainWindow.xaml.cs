16006af639fdcc200aa5cd4da6397a204d132d5d
pseudostripy <pseudostripy@gmail.com>
2023-01-25 00:41:33 -0500

    QOL: Make stat editing enabled by default

--- DS2S META/MainWindow.xaml.cs
+++ DS2S META/MainWindow.xaml.cs
@@ -4,26 +4,14 @@ using PropertyHook;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
-using System.Collections.ObjectModel;
-using DS2S_META.ViewModels;
-using DesktopWPFAppLowLevelKeyboardHook;
+
 
 namespace DS2S_META
 {
--- DS2S META/Properties/Settings.Designer.cs
+++ DS2S META/Properties/Settings.Designer.cs
@@ -133,7 +133,7 @@ namespace DS2S_META.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool EditStats {
             get {
                 return ((bool)(this["EditStats"]));
