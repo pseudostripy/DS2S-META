6da4c982961402ca26501a9914cbdc7586fb97d8
Meander5 <meander55555@gmail.com>
2023-03-10 01:42:59 +0100

    Randomizer settings are now stored in their own XML file

--- DS2S META/TabControls/RandomizerSettings.xaml.cs
+++ DS2S META/TabControls/RandomizerSettings.xaml.cs
@@ -15,14 +15,49 @@ using System.Windows.Data;
 using Octokit;
 using System.ComponentModel;
 using DS2S_META.Properties;
+using System.Xml.Serialization;
+using System.Collections.Specialized;
 
 namespace DS2S_META
 {
-    /// <summary>
-    /// Randomizer Code & Front End for RandomizerSettings.xaml
-    /// </summary>
     public partial class RandomizerSettings : METAControl
     {
+        static readonly string SettingsFilePath = $"{GetTxtResourceClass.ExeDir}/Resources/RandomizerSettings.xml";
+
+
+        public static void WriteToXmlFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+        {
+            TextWriter writer = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                writer = new StreamWriter(filePath, append);
+                serializer.Serialize(writer, objectToWrite);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+
+        public static T ReadFromXmlFile<T>(string filePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                reader = new StreamReader(filePath);
+                return (T)serializer.Deserialize(reader);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+
+
         public class ItemPlacementRestrictionSettings : INotifyPropertyChanged
         {
             public static Dictionary<ItemRestrictionType, string> TypeComboItems { get; set; } = new() {
@@ -95,6 +130,23 @@ namespace DS2S_META
                 return MapAreaComboItems.Single(e => e.Key == area);
             }
 
+            public void UpdateRawEnumValues()
+            {
+                RawTypeValue = Type.Key;
+                RawAreaValue = Area.Key;
+            }
+
+            public void UpdateEnumValuesFromRaw()
+            {
+                Type = GetTypeComboItem(RawTypeValue);
+                Area = GetAreaComboItem(RawAreaValue);
+            }
+
+            // Those don't need to fire PropertyChangedEvents, since those cannot be changed in UI
+            public List<int> ItemIDs { get; set; }
+            public string Name { get; set; }
+
+            // Used for hiding the section for area selection and min/max distance
             private Visibility _areaSelectionVisible = Visibility.Collapsed;
             public Visibility AreaSelectionVisible
             {
@@ -105,15 +157,63 @@ namespace DS2S_META
                 }
             }
 
-            public List<int> ItemIDs { get; set; }
+            
+            // ComboBox selection for item restriction type
+            private KeyValuePair<ItemRestrictionType, string> type = GetTypeComboItem(ItemRestrictionType.Anywhere);
+            [XmlIgnore]
+            public KeyValuePair<ItemRestrictionType, string> Type
+            {
+                get => type; set
+                {
+                    type = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Type)));
+                }
+            }
+            public ItemRestrictionType RawTypeValue; // Bypass for unserializable KeyValuePair class
 
-            public KeyValuePair<ItemRestrictionType, string> Type { get; set; } = GetTypeComboItem(ItemRestrictionType.Anywhere);
+            // ComboBox selection for map area
+            private KeyValuePair<MapArea, string> area = GetAreaComboItem(MapArea.Majula);
+            [XmlIgnore]
+            public KeyValuePair<MapArea, string> Area
+            {
+                get => area; set
+                {
+                    area = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Area)));
+                }
+            }
+            public MapArea RawAreaValue; // Bypass for unserializable KeyValuePair class
 
-            public string Name { get; set; }
 
-            public KeyValuePair<MapArea, string> Area { get; set; } = GetAreaComboItem(MapArea.Majula);
-            public int AreaDistanceLowerBound { get; set; } = 0;
-            public int AreaDistanceUpperBound { get; set; } = 0;
+            // Min distance field value
+            private int areaDistanceLowerBound = 0;
+            public int AreaDistanceLowerBound
+            {
+                get => areaDistanceLowerBound; set
+                {
+                    areaDistanceLowerBound = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AreaSelectionVisible)));
+                }
+            }
+
+            // Max distance field value
+            private int areaDistanceUpperBound = 0;
+            public int AreaDistanceUpperBound
+            {
+                get => areaDistanceUpperBound; set
+                {
+                    areaDistanceUpperBound = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AreaSelectionVisible)));
+                }
+            }
+
+
+            // Necessary for deserialization
+            public ItemPlacementRestrictionSettings()
+            {
+                ItemIDs = new();
+                Name = string.Empty;
+            }
 
             public ItemPlacementRestrictionSettings(string name, List<int> itemIds, ItemRestrictionType defaultType = ItemRestrictionType.Anywhere, MapArea area = MapArea.Majula, int minDist = 0, int maxDist = 0)
             {
@@ -127,28 +227,40 @@ namespace DS2S_META
             }
         }
 
-        public static ObservableCollection<ItemPlacementRestrictionSettings> ItemRestrictions { get; set; } = new()
-        {
-            new ItemPlacementRestrictionSettings("Estus Flask", new(){60155000}, ItemRestrictionType.Anywhere),
-            new ItemPlacementRestrictionSettings("Ring of Binding", new(){ 40410000 }, ItemRestrictionType.Anywhere),
-            new ItemPlacementRestrictionSettings("Silvercat Ring", new(){ 40420000 }, ItemRestrictionType.Anywhere),
-            new ItemPlacementRestrictionSettings("Aged Feather", new(){ 60355000 }, ItemRestrictionType.Anywhere),
 
-            new ItemPlacementRestrictionSettings("Any Pyromancy Flame", new(){ 05400000, 05410000 }, ItemRestrictionType.Anywhere),
+        public static ObservableCollection<ItemPlacementRestrictionSettings> ItemRestrictions { get; set; }
 
-            new ItemPlacementRestrictionSettings("Dull Ember", new(){ 50990000 }, ItemRestrictionType.Anywhere),
-            new ItemPlacementRestrictionSettings("Lenigrast's Key", new(){ 50870000 }, ItemRestrictionType.Anywhere),
-            new ItemPlacementRestrictionSettings("Any Blacksmith Key", new(){ 50850000, 50870000, 50990000 }, ItemRestrictionType.Anywhere),
 
-            new ItemPlacementRestrictionSettings("Rotunda Lockstone", new(){ 50890000 }, ItemRestrictionType.Anywhere),
-            new ItemPlacementRestrictionSettings("King's Ring", new(){ 40510000 }, ItemRestrictionType.Anywhere),
-            new ItemPlacementRestrictionSettings("Ashen Mist Heart", new(){ 50910000 }, ItemRestrictionType.Anywhere),
-        };
-
-        // FrontEnd:
         public RandomizerSettings()
         {
             InitializeComponent();
+            try
+            {
+                ItemRestrictions = ReadFromXmlFile<ObservableCollection<ItemPlacementRestrictionSettings>>(SettingsFilePath);
+                // KeyValuePairs cannot be (de)serialized, so the area enum value is stored in an individual serializable variable
+                foreach (var restriction in ItemRestrictions) restriction.UpdateEnumValuesFromRaw();
+            }
+            catch (FileNotFoundException)
+            {
+                ItemRestrictions = new() {
+                    new ItemPlacementRestrictionSettings("Estus Flask", new(){60155000}, ItemRestrictionType.Anywhere),
+                    new ItemPlacementRestrictionSettings("Ring of Binding", new(){ 40410000 }, ItemRestrictionType.Anywhere),
+                    new ItemPlacementRestrictionSettings("Silvercat Ring", new(){ 40420000 }, ItemRestrictionType.Anywhere),
+                    new ItemPlacementRestrictionSettings("Aged Feather", new(){ 60355000 }, ItemRestrictionType.Anywhere),
+
+                    new ItemPlacementRestrictionSettings("Any Pyromancy Flame", new(){ 05400000, 05410000 }, ItemRestrictionType.Anywhere),
+
+                    new ItemPlacementRestrictionSettings("Dull Ember", new(){ 50990000 }, ItemRestrictionType.Anywhere),
+                    new ItemPlacementRestrictionSettings("Lenigrast's Key", new(){ 50870000 }, ItemRestrictionType.Anywhere),
+                    new ItemPlacementRestrictionSettings("Any Blacksmith Key", new(){ 50850000, 50870000, 50990000 }, ItemRestrictionType.Anywhere),
+
+                    new ItemPlacementRestrictionSettings("Rotunda Lockstone", new(){ 50890000 }, ItemRestrictionType.Anywhere),
+                    new ItemPlacementRestrictionSettings("King's Ring", new(){ 40510000 }, ItemRestrictionType.Anywhere),
+                    new ItemPlacementRestrictionSettings("Ashen Mist Heart", new(){ 50910000 }, ItemRestrictionType.Anywhere),
+                };
+            }
+
+            foreach (var restriction in ItemRestrictions) restriction.PropertyChanged += SaveRandomizerSettings;
         }
 
         private void RestrictionTypeSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -156,6 +268,12 @@ namespace DS2S_META
             var box = sender as ComboBox;
             var context = box.DataContext as ItemPlacementRestrictionSettings;
             context.AreaSelectionVisible = context.Type.Key == ItemRestrictionType.AreaDistance ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void SaveRandomizerSettings(object? sender, PropertyChangedEventArgs e)
+        {
+            foreach (var restriction in ItemRestrictions) restriction.UpdateRawEnumValues();
+            WriteToXmlFile(SettingsFilePath, ItemRestrictions);
         }
     }
 }
