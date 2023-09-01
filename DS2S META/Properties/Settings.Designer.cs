c48d4840e1e8245c0044cc559bd77e60e25c773e
pseudostripy <pseudostripy@gmail.com>
2023-09-01 17:21:33 -0400

    Complete the conflict merge by fixing hotkeys/settings

--- DS2S META/Properties/Settings.Designer.cs
+++ DS2S META/Properties/Settings.Designer.cs
@@ -12,9 +12,7 @@ namespace DS2S_META.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
-
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -471,7 +469,6 @@ namespace DS2S_META.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-
         [global::System.Configuration.DefaultSettingValueAttribute("27")]
         public int hkyNoDeath {
             get {
@@ -479,7 +476,11 @@ namespace DS2S_META.Properties {
             }
             set {
                 this["hkyNoDeath"] = value;
-
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool RandoRaceMode {
             get {
